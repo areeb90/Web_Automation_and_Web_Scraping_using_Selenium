ffad706537341a3a80a23883af0694e3cb6190d5
Areeb Bin Azim <azim4204261@cloud.neduet.edu.pk>
2023-08-23 23:28:59 +0500

    started

--- gettingStarted.py
+++ gettingStarted.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome import service
+import time
+
 
 driver = webdriver.Chrome()
 driver.get("http://www.python.org")
@@ -10,5 +12,7 @@ elem = driver.find_element(By.NAME, "q")
 elem.clear()
 elem.send_keys("python")
 elem.send_keys(Keys.RETURN)
+time.sleep(5)  # Keep the window open for 5 seconds
+
 assert "No results found." not in driver.page_source
 driver.close()
